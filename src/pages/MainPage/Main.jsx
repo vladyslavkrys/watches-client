569ed0881a3cb516075118e116ea3ad91e7e00c3
Vladyslav Krys <vladyslav.krys@signanthealth.com>
2023-02-25 11:07:44 +0100

    feature: Mobile header.

--- src/pages/MainPage/Main.jsx
+++ src/pages/MainPage/Main.jsx
@@ -1,9 +1,11 @@
 import logo from './assets/logo.svg';
 import './assets/Main.css';
+import { MainMenuHeader } from '../../components/MainMenuHeader/MainMenuHeader';
 
 const Main = () => (
   <div className="Main">
-    <header className="Main-header">
+    <MainMenuHeader />
+    <div className="Main-header">
       <img src={logo} className="Main-logo" alt="logo" />
       <p>
         Edit <code>src/components/Main.js</code> and save to reload.
@@ -11,7 +13,7 @@ const Main = () => (
       <a className="Main-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
-    </header>
+    </div>
   </div>
 );
 
---
+++ src/shared/components/svg/SearchIconSvg.jsx
@@ -0,0 +1,14 @@
+const SearchIconSvg = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M10.5 16.25C13.6756 16.25 16.25 13.6756 16.25 10.5C16.25 7.32436 13.6756 4.75 10.5 4.75C7.32436 4.75 4.75 7.32436 4.75 10.5C4.75 13.6756 7.32436 16.25 10.5 16.25Z"
+        stroke="white"
+        strokeWidth="1.5"
+      />
+      <path d="M14.104 14.7461L19.104 19.7461" stroke="white" strokeWidth="1.5" />
+    </svg>
+  );
+};
+
+export default SearchIconSvg;
